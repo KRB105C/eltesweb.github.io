dae97217ea409507a44cdb75b731617d963654d4
KRB105C <126673026+KRB105C@users.noreply.github.com>
2024-09-13 18:09:52 +0800

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,11 +1,10 @@
-
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 
-export default async function handler(req, res) {
-
 export async function GET(request) {
+  // Ambil parameter URL dari query string
   const url = new URL(request.url).searchParams.get('url');
+  
   if (!url) {
     return NextResponse.json({ error: 'URL is required' }, { status: 400 });
   }
@@ -15,6 +14,7 @@ export async function GET(request) {
     const response = await axios.get(url);
     return NextResponse.json(response.data);
   } catch (error) {
-    return NextResponse.json({ error: 'Error occurred' }, { status: 500 });
+    console.error('Error fetching data:', error);
+    return NextResponse.json({ error: 'Error occurred while fetching data' }, { status: 500 });
   }
 }
